5baf20f04173f05ae439dd253d4f74f29e79a7bf
MaxRobinsonTheGreat <maxdrob1@gmail.com>
2018-02-20 15:01:14 -0700

    basic server correction

--- client_game.js
+++ client_game.js
@@ -11,7 +11,9 @@ var key = {left:false, right:false, up:false, down:false}
 var last_update = 0;
 var delta_time = 0;
 
-var socket = io.connect('http://10.24.16.60:4200/');
+var prediction_queue = [];
+
+var socket = io.connect('http://10.24.68.56:4200/');
 socket.on('connect', function(data) {
    socket.emit('join', 'Hello World from client');
    main();
@@ -34,13 +36,30 @@ function Update(){
   last_update = Date.now();
 
   box = core.moveBox(box, key, delta_time);
+  var boundry_result = core.checkBoundry(box);
+  box = boundry_result.box;
+  if(boundry_result.was_correction){
+    console.log(boundry_result.box);
+    prediction_queue.push(boundry_result.box);
+  }
 
-  // var pack = {moves: key, timestamp:Date.now()}
-  // socket.emit('update', pack);
+  var pack = {moves: key, timestamp:Date.now()};
+  socket.emit('move', pack);
 }
 socket.on('all', function(data) {
     others = data;
     // console.log(data);
+});
+socket.on('correction', function(new_box){
+  box = new_box;
+  console.log("correction");
+});
+socket.on('boundry', function(new_box){
+  // var prediction = prediction_queue.shift();
+  // //console.log(new_box +", "+ prediction);
+  // if(prediction !== new_box){
+  //   box = new_box;
+  // }
 });
 
 
@@ -68,8 +87,7 @@ function handleKeyDown(evt) {
   if ( evt.keyCode == KEY_DOWN )
     key.down = true;
 
-  var pack = {moves: key, timestamp:Date.now()}
-  socket.emit('update', pack);
+
 }
 function handleKeyUp(evt){
   evt.preventDefault();
@@ -82,6 +100,6 @@ function handleKeyUp(evt){
   if ( evt.keyCode == KEY_DOWN )
     key.down = false;
 
-  var pack = {moves: key, timestamp:Date.now()}
-  socket.emit('update', pack);
+  var pack = {box: box, moves: key, timestamp:Date.now()};
+  socket.emit('stop', pack);
 }
